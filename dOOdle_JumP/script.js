6125204a6a87016b74af6ee1f739e8d95b02d230
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-02-05 21:50:29 +0530

    .

---
+++ dOOdle_JumP/script.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  const doodler = document.createElement('div')
+  let isGameOver = false
+  let speed = 3
+  let platformCount = 5
+  let platforms = []
+  let score = 0
+  let doodlerLeftSpace = 50
+  let startPoint = 150
+  let doodlerBottomSpace = startPoint
+  const gravity = 0.9
+  let upTimerId
+  let downTimerId
+  let isJumping = true
+  let isGoingLeft = false
+  let isGoingRight = false
+  let leftTimerId
+  let rightTimerId
+
+  class Platform {
+    constructor(newPlatBottom) {
+      this.left = Math.random() * 315
+      this.bottom = newPlatBottom
+      this.visual = document.createElement('div')
+
+      const visual = this.visual
+      visual.classList.add('platform')
+      visual.style.left = this.left + 'px'
+      visual.style.bottom = this.bottom + 'px'
+      grid.appendChild(visual)
+    }
+  }
+
+
+  function createPlatforms() {
+    for(let i =0; i < platformCount; i++) {
+      let platGap = 600 / platformCount
+      let newPlatBottom = 100 + i * platGap
+      let newPlatform = new Platform (newPlatBottom)
+      platforms.push(newPlatform)
+      console.log(platforms)
+    }
+  }
+
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+        platforms.forEach(platform => {
+          platform.bottom -= 4
+          let visual = platform.visual
+          visual.style.bottom = platform.bottom + 'px'
+
+          if(platform.bottom < 10) {
+            let firstPlatform = platforms[0].visual
+            firstPlatform.classList.remove('platform')
+            platforms.shift()
+            console.log(platforms)
+            score++
+            var newPlatform = new Platform(600)
+            platforms.push(newPlatform)
+          }
+      }) 
+    }
+    
+  }
+
+  function createDoodler() {
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodlerLeftSpace = platforms[0].left
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+  }
+
+function fall() {
+  isJumping = false
+    clearInterval(upTimerId)
+    downTimerId = setInterval(function () {
+      doodlerBottomSpace -= 5
+      doodler.style.bottom = doodlerBottomSpace + 'px'
+      if (doodlerBottomSpace <= 0) {
+        gameOver()
+      }
+      platforms.forEach(platform => {
+        if (
+          (doodlerBottomSpace >= platform.bottom) &&
+          (doodlerBottomSpace <= (platform.bottom + 15)) &&
+          ((doodlerLeftSpace + 60) >= platform.left) && 
+          (doodlerLeftSpace <= (platform.left + 85)) &&
+          !isJumping
+          ) {
+            console.log('tick')
+            startPoint = doodlerBottomSpace
+            jump()
+            console.log('start', startPoint)
+            isJumping = true
+          }
+      })
+
+    },20)
+}
+
+  
